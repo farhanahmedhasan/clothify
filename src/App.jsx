3cae9174dd0277670fa93da99ac1d7123c529989
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-04-08 13:11:41 +0600

    Added categories layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+import './categories.styles.scss';
+
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/mens.png',
+    },
+  ];
+
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img src="" alt="" /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({ id, title, imageUrl }) => (
+        <div key={id} className='category-container'>
+          <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+          {/* <img src={imageUrl} alt={`${title}-image`} /> */}
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
