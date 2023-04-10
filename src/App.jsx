6ebf31af6b6caccfa8dd5f8d1c0d83054e6d4d80
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-04-11 02:04:41 +0600

    Using the react-router-dom Outlet pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import HomePage from './pages/home/HomePage';
+
+function MainNavigation() {
+  return (
+    <nav>
+      <div>I am the Navigation Bar</div>
+      <Outlet />
+    </nav>
+  );
+}
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      <Route path='/' element={<MainNavigation />}>
+        <Route index element={<HomePage />} />
+        <Route path='shop' element={<div>I am Shop Page</div>} />
+
+        <Route path='*' element={<div>Not Found</div>} />
+      </Route>
     </Routes>
   );
 }
